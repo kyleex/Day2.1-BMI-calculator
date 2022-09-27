cbfcd8e7180a425fcd05d1dc8eff110cf7e4a01a
Anton WILLIAMS <antwill34@icloud.com>
2022-09-27 02:57:30 +0200

    Create main.py
    main script

---
+++ main.py
@@ -0,0 +1,9 @@
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+
+float_height = float(height)
+int_weight = int(weight)
+
+BMI = int(int_weight / (float_height**2))
+
+print(BMI)
